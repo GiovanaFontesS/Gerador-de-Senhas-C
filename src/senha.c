4a07a6f0fb2a3aad7ed7da13f7f96ff10fdab059
Giovana Fontes <149431855+GiovanaFontesS@users.noreply.github.com>
2026-07-14 09:04:53 -0300

    Implement password generation function

--- src/senha.c
+++ src/senha.c
@@ -1,1 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
+#include "../include/senha.h"
+
+void gerarSenha(int tamanho,
+                int maiusculas,
+                int minusculas,
+                int numeros,
+                int simbolos)
+{
+    char caracteres[100] = "";
+    char senha[100];
+
+    if (maiusculas)
+        strcat(caracteres, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+
+    if (minusculas)
+        strcat(caracteres, "abcdefghijklmnopqrstuvwxyz");
+
+    if (numeros)
+        strcat(caracteres, "0123456789");
+
+    if (simbolos)
+        strcat(caracteres, "!@#$%&*+-=?");
+
+    if (strlen(caracteres) == 0)
+    {
+        printf("\nErro: nenhum tipo de caractere foi selecionado.\n");
+        return;
+    }
+
+    srand(time(NULL));
+
+    int quantidade = strlen(caracteres);
+
+    for (int i = 0; i < tamanho; i++)
+    {
+        int indice = rand() % quantidade;
+        senha[i] = caracteres[indice];
+    }
+
+    senha[tamanho] = '\0';
+
+    printf("\n=====================================\n");
+    printf("Senha gerada: %s\n", senha);
+    printf("=====================================\n");
+}
