507010c6b89b57f887ae8c176e45ae49835d64c7
Giovana Fontes <149431855+GiovanaFontesS@users.noreply.github.com>
2026-07-14 08:59:49 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main() {
+    int tamanho;
+
+    printf("=====================================\n");
+    printf("      GERADOR DE SENHAS EM C\n");
+    printf("=====================================\n\n");
+
+    printf("Digite o tamanho da senha: ");
+    scanf("%d", &tamanho);
+
+    printf("\nVoce escolheu uma senha com %d caracteres.\n", tamanho);
+
+    return 0;
+}
